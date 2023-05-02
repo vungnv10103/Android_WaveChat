32fe3438fae4a8b13995ce8d905502f6b33fbca0
VungNguyen <vungnguyenn1001@gmail.com>
2023-05-03 02:07:10 +0700

    update UX

---
+++ app/src/main/java/com/vungnv/chatapp/utils/Constants.java
@@ -0,0 +1,16 @@
+package com.vungnv.chatapp.utils;
+
+public class Constants {
+    public static final String KEY_COLLECTION_USER = "users";
+    public static final String KEY_NAME = "name";
+    public static final String KEY_PHONE = "phoneNumber";
+    public static final String KEY_EMAIL = "email";
+    public static final String KEY_PASSWORD = "password";
+    public static final String KEY_USER_ID = "userId";
+    public static final String KEY_IMAGE = "image";
+    public static final String KEY_IS_SIGNED_IN = "isSignedIn";
+    public static final String KEY_PREFERENCE_NAME = "chatAppPreference";
+    public static final String HEX_CHAR = "0123456789ABCDEF";
+    public static final String TAG = "Log";
+    public static final String CHANNEL_ID = "my_channel_id";
+}
---
+++ app/src/main/java/com/vungnv/chatapp/utils/HashPassword.java
@@ -0,0 +1,28 @@
+package com.vungnv.chatapp.utils;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class HashPassword{
+
+    private static final char[] HEX_ARRAY = Constants.HEX_CHAR.toCharArray();
+
+    public static String hashPassword(String password) throws NoSuchAlgorithmException {
+        MessageDigest digest = MessageDigest.getInstance("SHA-256");
+        byte[] encodedHash = digest.digest(password.getBytes(StandardCharsets.UTF_8));
+        return bytesToHex(encodedHash);
+    }
+
+    private static String bytesToHex(byte[] bytes) {
+        char[] hexChars = new char[bytes.length * 2];
+        for (int i = 0; i < bytes.length; i++) {
+            int v = bytes[i] & 0xFF;
+            hexChars[i * 2] = HEX_ARRAY[v >>> 4];
+            hexChars[i * 2 + 1] = HEX_ARRAY[v & 0x0F];
+        }
+        return new String(hexChars);
+    }
+}
+
+
